21e58ec8bef2b8c1eed8a0137227ea9d5625e3cb
prakarshini <prakarshinimummareddy@gmail.com>
2026-08-05 11:52:19 +0530

    hello

---
+++ sub.py
@@ -0,0 +1,9 @@
+# Input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Subtract the numbers
+difference = num1 - num2
+
+# Display the result
+print("The difference is:", difference)
